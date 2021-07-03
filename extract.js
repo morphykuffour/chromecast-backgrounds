405ea27c4daa2e94db73bae7892bad36b10e3ffd
Morphy Kuffour <morpkuff@gmail.com>
2021-07-03 13:48:05 -0400

    added bash suppport to download all images then optionally rename

--- extract.js
+++ extract.js
@@ -1,28 +1,16 @@
 // file system
 const Fs = require('fs');
-const Axios = require('axios');
-const Path = require('path');
 
 // Load json file
 const jsonFile = Fs.readFileSync('./backgrounds.json', 'utf8');
 const jsonData = JSON.parse(jsonFile);
 
-// function downloadfiles(){
-//     var len = jsonData.length;
-
-//     for(let i = 0; i < len; i++){
-//       download(jsonData[i].url);
-//     }
-// }
-
-// downloadfiles();
+var len = jsonData.length;
 
 for (let i = 0; i < len; i++) {
-    var len = jsonData.length;
     console.log(jsonData[i].url);
 }
 
 // console.log(jsonData[0].author); 
 // console.log(jsonData[1].author);
 // console.log(jsonData[1].url);
-
